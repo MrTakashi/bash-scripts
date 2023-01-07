909440604952250196b7e7484b7e94fa870d19ab
MK <mihael.kartashev@gmail.com>
2023-01-07 23:36:15 +0300

    ..

--- change-hostname.sh
+++ change-hostname.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 
 if [ -z "$1" ]; then
-    echo "Usage: $0 <new-host name>"
-    echo
     echo "[Host name changer script]"
     echo
-    echo "script execute hostnamectl set-hostname "
+    echo "Usage: $0 <new-hostname>"
+    echo
+    echo "script will execute <hostnamectl set-hostname new-hostname>>"
     echo "and do changes in /etc/hosts"
 else
-    echo "# try to set new host name with command <hostnamectl set-hostname $1>";
+    echo "# try to set new hostname with command <hostnamectl set-hostname $1>";
     hostnamectl set-hostname "$1"
     echo
+    echo "uname -a"
     uname -a
-    echo
     printf "# try to update /etc/hosts with command <sed -i.bak -r s/^127.0.1.1.*/127.0.1.1\t%s.local\t%s/ /etc/hosts>", "$1" "$1";
     sed -i.bak -r "s/^127.0.1.1.*/127.0.1.1\t$1.local\t$1/" /etc/hosts
     echo
+    echo "cat /etc/hosts"
     cat /etc/hosts
     echo
-    echo "if all seems correct, run: shutdown -r now"
+    echo "If all seems correct, run: shutdown -r now"
     echo
 fi
