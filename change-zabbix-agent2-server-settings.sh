1de5294bc29f3cb22d6658177b0358efa88975d7
MK <mihael.kartashev@gmail.com>
2023-02-26 00:27:01 +0300

    update raw-link

--- change-zabbix-agent2-server-settings.sh
+++ change-zabbix-agent2-server-settings.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ###########################################################################################
-# wget -qO - https://raw.githubusercontent.com/MrTakashi/bash-scripts/c01a68534e8b3f5696139bd27da7129182404045/change-zabbix-agent2-server-settings.sh | bash -s %new_value_for_Server=%
+# wget -qO - https://raw.githubusercontent.com/MrTakashi/bash-scripts/master/change-zabbix-agent2-server-settings.sh | bash -s %new_value_for_Server=%
 
 
 if [ -z "$1" ]; then
